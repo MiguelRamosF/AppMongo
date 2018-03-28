542a48849068d512c5c34913368fa56fa0d65078
Miguel Ramos <miguel-ramos@hotmail.fr>
2018-03-29 01:22:28 +0200

    feat(searchfilter,sorting,add,design)

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,8 @@ const express = require ('express');
 const router = express.Router();
 const Cities = require('../models/cities');
 
-// get a list of cities from the db
-router.get('/cities', function(req, res, next){
-    /* Ninja.find({}).then(function(ninjas){
-        res.send(ninjas);
-    }); */
+// get a list of cities with the function geoNear from the db
+router.get('/cities/near', function(req, res, next){
     Cities.aggregate(
 
         [
@@ -25,6 +22,40 @@ router.get('/cities', function(req, res, next){
         res.send(city);
     }).catch(next);
 });
+
+// get a list of cities based on filters from the db
+router.get('/cities/all', function(req, res, next){
+
+    var query = "{"
+    if(req.query.name){
+        query+="\"name\":\""+req.query.name+"\""
+    }
+    if(req.query.country){
+        query+="\"country\":\""+req.query.country+"\""
+    }
+    if(req.query.population){
+        query+="\"population\":"+req.query.population+""
+    }
+    if(req.query.timeZone){
+        query+="\"timeZone\":\""+req.query.timeZone+"\""
+    }
+    /*if(!req.query.name && !req.query.country && !req.query.population && !req.query.timeZone){
+
+
+    }*/
+    query+="}";
+    var formated_query = query.replace(/(["])(["])/g, '$1,$2');
+    //console.log(formated_query);
+    var object_query = JSON.parse(formated_query);
+    //console.log(object_query);
+    Cities.find(
+        object_query
+
+    ).then(function(city){
+        res.send(city);
+    }).catch(next);
+});
+
 
 // add a new city to the db
 router.post('/cities', function(req, res, next){
